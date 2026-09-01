1b0bc461b9e9aaca5146e6e145ce0b8bcaff2969
Renan <renan.gre@gmail.com>
2026-09-01 14:52:26 -0300

    Criando tela de conversa com IA, e criando conexão com API da OpenAi para uso local no PC, sem finalidade de usar em qualquer navegador.

--- dash.js
+++ dash.js
@@ -365,3 +365,106 @@ document.getElementById('btnsavetour').addEventListener('click', ()=>{
 
     })
 });
+
+//Página da IA
+
+const inputQuestion = document.getElementById('inputQuestion');
+const result = document.getElementById('result');
+
+inputQuestion.addEventListener('keypress', (e) => {
+
+    if (inputQuestion.value && e.key === "Enter") {
+        SendQuestion();
+    }
+
+});
+
+const OPENAI_API_KEY = "";
+
+
+function SendQuestion() {
+
+    var sQuestion = inputQuestion.value;
+
+    fetch("https://api.openai.com/v1/responses", {
+
+        method: "POST",
+
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + OPENAI_API_KEY
+        },
+
+        body: JSON.stringify({
+            model: "gpt-5.6-luna",
+            input: sQuestion,
+        })
+
+    })
+
+    .then((response) => response.json())
+
+    .then((json) => {
+
+        if (result.value) {
+            result.value += "\n";
+        }
+
+        if (json.error?.message) {
+
+            result.value += `Error: ${json.error.message}`;
+
+        } else if (json.output_text) {
+
+            result.value += "IA: " + json.output_text;
+
+        } else {
+
+            result.value += "Sem resposta.";
+
+        }
+
+        result.scrollTop = result.scrollHeight;
+
+    })
+
+    .catch((error) => {
+
+        console.error("Error:", error);
+
+        result.value += "\nErro ao conectar com a IA.";
+
+    })
+
+    .finally(() => {
+
+        inputQuestion.value = "";
+        inputQuestion.disabled = false;
+        inputQuestion.focus();
+
+    });
+
+
+    if (result.value) {
+        result.value += "\n\n\n";
+    }
+
+    result.value += `Eu: ${sQuestion}`;
+
+    inputQuestion.value = "Carregando...";
+    inputQuestion.disabled = true;
+
+}
+
+document.getElementById('IA').addEventListener('click', (e)=>{
+    e.preventDefault;
+
+    const IAopen = document.getElementById('IAopen');
+
+    IAopen.classList.add('active');
+    const IAvoltar = document.getElementById('IAvoltar').addEventListener('click', ()=> {
+
+        IAopen.classList.remove('active');
+    })
+})
